821cd2bc167b311a39ee3df865e5d1eb98786de0
Nicolapsq <nicolapasqua84@gmail.com>
2025-05-26 16:38:57 +0200

    Add react-form whid object and trim metod

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,22 +2,29 @@ import { useState } from "react";
 
 export default function List() {
   const [listaArticoli, setListaArticoli] = useState([
-    "articolo-1",
-    "articolo-2",
-    "articolo-3",
-    "articolo-4",
-    "articolo-5",
+    { id: 1, titolo: "articolo-1" },
+    { id: 2, titolo: "articolo-2" },
+    { id: 3, titolo: "articolo-3" },
+    { id: 4, titolo: "articolo-4" },
+    { id: 5, titolo: "articolo-5" },
   ]);
   const [nuovoArticolo, setNuovoArticolo] = useState("");
 
   const aggiungiArticolo = (e) => {
     e.preventDefault();
 
-    if (nuovoArticolo !== "") {
-        const nuovaListaArticoli = [...listaArticoli, nuovoArticolo];
-        setListaArticoli(nuovaListaArticoli);
-        console.table(nuovaListaArticoli);
-        setNuovoArticolo("");
+    if (nuovoArticolo.trim() !== "") {
+      const nuovoId = listaArticoli[listaArticoli.length - 1].id + 1;
+
+        const articolo = {
+        id: nuovoId,
+        titolo: nuovoArticolo
+      };
+
+      const nuovaListaArticoli = [...listaArticoli, articolo];
+      setListaArticoli(nuovaListaArticoli);
+      console.table(nuovaListaArticoli);
+      setNuovoArticolo("");
     }
   };
 
@@ -26,9 +33,9 @@ export default function List() {
       <div className="container">
         <h1>Lista Articoli</h1>
         <ul className="list">
-          {listaArticoli.map((articolo, index) => (
-            <li key={index} className="list-item">
-              {articolo}
+          {listaArticoli.map((articolo,) => (
+            <li key={articolo.id} className="list-item">
+              {articolo.titolo}
             </li>
           ))}
         </ul>
